789c81ffe8244ad622bcc1186150a914afa07bd4
PidgeonEEEB <paviaman2002@hotmail.com>
2024-10-03 10:32:43 +0200

    Add project files.

---
+++ association/Grade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace association
+{
+    internal class Grade
+    {
+        public int grade = 1;
+    }
+}
---
+++ association/GradeList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace association
+{
+    class GradeList
+    {
+        private int Size;
+        private List<Grade> grades = new List<Grade> ();
+
+        public GradeList(int maxNumberOfGrades)
+        {
+
+        }
+        public int GetSize()
+        {
+            //man SKAL returnere noget når man har en variabel som int.
+            return 1;
+        }
+        public Grade getGrade(int index)
+            {
+                return grades[index];
+            }
+        public void addGrade(Grade grade)
+        {
+             grades.Add(grade);
+        }
+        public List<Grade> getAllGrades()
+        {   
+            return grades;
+        }
+        public List<Grade> getAverage()
+        {
+            return Math.Round(Convert.ToDouble);
+        }
+
+    }
+
+
+}
